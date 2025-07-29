4ce6ab563b45e276b9735d37a02a74f4ea9782a5
Alan <161913026+Pratyush827@users.noreply.github.com>
2025-07-29 23:01:27 +0545

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,11 @@ import dotenv from 'dotenv';
 import {connectDB} from "./config/db.js"
 import todoRoutes from "./routes/todo.route.js"
 import cors from 'cors';
-import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 dotenv.config();
 
